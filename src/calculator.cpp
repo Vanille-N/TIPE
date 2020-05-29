320b40008acfbb747e1ecb4c2649ef6bbb363ca8
Vanille-N <neven.villani@gmail.com>
2020-05-29 14:59:39 +0200

    Comments for calculator

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,5 +1,11 @@
 #include "calculator.h"
 
+/* The object responsible for all important calculations.
+ * (i.e. it knows how to integrate the system of differential equations
+ * that emerges from the model.
+ * This file also contains information on how to calculate water density.
+ */
+
 Calculator::Calculator (QWidget * parent, QVector<State *> & Stt, QVector<Const *> & Cst, Const * Aim, bool * pcpl, bool * peqm) {
     m_Stt = Stt ;
     m_Cst = Cst ;
@@ -9,9 +15,12 @@ Calculator::Calculator (QWidget * parent, QVector<State *> & Stt, QVector<Const 
     m_refreshRoot = parent ;
 }
 
+/* Advance the whole system by one unit of time
+ */
 void Calculator::step () {
 #if INTEGRATE_PHI == 0
     for (int K = 0; K < NB_POINTS; K++) {
+        // Bring all useful variable into scope
         double _Te = m_Stt[K]->getT(equator) ;
         double _Tp = m_Stt[K]->getT(pole) ;
         double _Se = m_Stt[K]->getS(equator) ;
@@ -25,13 +34,18 @@ void Calculator::step () {
         double _tauT_ = m_Cst[K]->get(tauT) ;
         double _tauS_ = m_Cst[K]->get(tauS) ;
         double _dt_ = m_Cst[K]->get(dt) ;
+        // Calculate densities
         double sigP = sigma(_Sp, _Tp) ;
         double sigE = sigma(_Se, _Te) ;
+        // Calculate variations of the relevant quantities
+        // Note that CPL and EQM are bool*, so having them as multiplying
+        // factors is an easy way to negate certain effects
         double phi = *CPL * _lambda_ * (sigP - sigE) ;
         double dTe = *CPL * (_Tp - _Te) * (abs(phi) / _M0_) + *EQM * (_Te_ - _Te) / _tauT_ ;
         double dTp = *CPL * (_Te - _Tp) * (abs(phi) / _M0_) + *EQM * (_Tp_ - _Tp) / _tauT_ ;
         double dSe = *CPL * (_Sp - _Se) * (abs(phi) / _M0_) + *EQM * (_Se_ - _Se) / _tauS_ ;
         double dSp = *CPL * (_Se - _Sp) * (abs(phi) / _M0_) + *EQM * (_Sp_ - _Sp) / _tauS_ ;
+        // Update the state of the simulation
         m_Stt[K]->setPhi(phi) ;
         m_Stt[K]->setS(pole, _Sp + dSp*_dt_) ;
         m_Stt[K]->setS(equator, _Se + dSe*_dt_) ;
@@ -55,6 +69,7 @@ void Calculator::step () {
     }
 #elif INTEGRATE_PHI == 1
     for (int K = 0; K < NB_POINTS; K++) {
+        // Bring all relevant quantities into scope
         double _Te0 = m_Stt[K]->getT(equator) ;
         double _Tp0 = m_Stt[K]->getT(pole) ;
         double _Se0 = m_Stt[K]->getS(equator) ;
@@ -68,29 +83,41 @@ void Calculator::step () {
         double _tauT_ = m_Cst[K]->get(tauT) ;
         double _tauS_ = m_Cst[K]->get(tauS) ;
         double _dt_ = m_Cst[K]->get(dt) ;
+        // Calculate first approximation of densities
         double sigP0 = sigma(_Sp0, _Tp0) ;
         double sigE0 = sigma(_Se0, _Te0) ;
+        // Calculate first approximation of variations
+        // Note that CPL and EQM are bool*, so having them as multiplying
+        // factors is an easy way to negate certain effects
         double phi0 = *CPL * _lambda_ * (sigP0 - sigE0) ;
         double dTe0 = *CPL * (_Tp0 - _Te0) * (abs(phi0) / _M0_) + *EQM * (_Te_ - _Te0) / _tauT_ ;
         double dTp0 = *CPL * (_Te0 - _Tp0) * (abs(phi0) / _M0_) + *EQM * (_Tp_ - _Tp0) / _tauT_ ;
         double dSe0 = *CPL * (_Sp0 - _Se0) * (abs(phi0) / _M0_) + *EQM * (_Se_ - _Se0) / _tauS_ ;
         double dSp0 = *CPL * (_Se0 - _Sp0) * (abs(phi0) / _M0_) + *EQM * (_Sp_ - _Sp0) / _tauS_ ;
+        // Calculate first approximation of the end values of quantities
         double _Te1 = _Te0 + dTe0 ;
         double _Tp1 = _Tp0 + dTp0 ;
         double _Se1 = _Se0 + dSe0 ;
         double _Sp1 = _Sp0 + dSp0 ;
+        // Calculate second approximation of the densities
         double sigP1 = sigma(_Sp1, _Tp1) ;
         double sigE1 = sigma(_Se1, _Te1) ;
+        // Calculate second approximation of the flow
         double phi1 = *CPL * _lambda_ * (sigP1 - sigE1) ;
+        // The actual flow is approximated by the mean of the two previously
+        // calculated values, so are all other quantities, ...
         double phi2 = (phi0 + phi1) / 2 ;
         double _Te2 = (_Te0 + _Te1) / 2 ;
         double _Tp2 = (_Tp0 + _Tp1) / 2 ;
         double _Se2 = (_Se0 + _Se1) / 2 ;
         double _Sp2 = (_Sp0 + _Sp1) / 2 ;
+        // ...And the same calculations are reiterated with the new
+        // better approximation
         double dTe2 = *CPL * (_Tp2 - _Te2) * (abs(phi2) / _M0_) + *EQM * (_Te_ - _Te2) / _tauT_ ;
         double dTp2 = *CPL * (_Te2 - _Tp2) * (abs(phi2) / _M0_) + *EQM * (_Tp_ - _Tp2) / _tauT_ ;
         double dSe2 = *CPL * (_Sp2 - _Se2) * (abs(phi2) / _M0_) + *EQM * (_Se_ - _Se2) / _tauS_ ;
         double dSp2 = *CPL * (_Se2 - _Sp2) * (abs(phi2) / _M0_) + *EQM * (_Sp_ - _Sp2) / _tauS_ ;
+        // Final update of the state of the simulation
         m_Stt[K]->setPhi(phi0) ;
         m_Stt[K]->setS(pole, _Sp0 + dSp2*_dt_) ;
         m_Stt[K]->setS(equator, _Se0 + dSe2*_dt_) ;
@@ -119,12 +146,13 @@ void Calculator::step () {
 
 
 #if SWEOS == 0
-
+// Linear approximation
 double sigma (double S, double T) {
     return RHO * (-ALPHA*(T-T0) + BETA*(S-S0)) ;
 }
 
 #elif SWEOS == 1
+// Polynomial fit
 double sq (double x) {
     return x * x ;
 }
