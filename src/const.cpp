3c88257177c047a66de51300e8385e8dc32b5dcf
Vanille-N <neven.villani@gmail.com>
2020-05-30 09:06:41 +0200

    added comments for  const

--- src/const.cpp
+++ src/const.cpp
@@ -1,5 +1,16 @@
 #include "const.h"
 
+/* A Const object is responsible for abstracting the management and
+ * retrieval of "constants". These are not truly constants because the whole
+ * point of this simulation is to observe how changes in their value affect
+ * the rest of the parameters.
+ * However, calling them constants is still appropriate for two reasons:
+ *    1. In the analytical study they were considered constants
+ *    2. The speed at which they change is low compared to other quantities
+ * They are loaded and stored in an array, but an enum enables adressing them
+ * with sensible names.
+ */
+
 Const::Const (bool variable) {
     for (int c = 0; c < NB_CST; c++) {
         m_constants[c] = 111.1 ;
@@ -13,6 +24,8 @@ Const::Const (bool variable) {
 #endif
 }
 
+/* Load save file from its name
+ */
 void Const::read (QString input) {
     QFile f (input) ;
     if (!f.open(QIODevice::ReadOnly)) return ;
@@ -23,6 +36,8 @@ void Const::read (QString input) {
     }
 }
 
+/* Write to a given file
+ */
 void Const::write (QString output) {
     QFile f (output) ;
     if (!f.open(QIODevice::WriteOnly)) return ;
@@ -40,6 +55,9 @@ void Const::set (cst c, double val) {
     m_constants[c] = val * m_offset[c] ;
 }
 
+/* The variation of these constants can be smoothened in one of two ways:
+ * exponential convergence or integrated exponential convergence.
+ */
 void Const::aim (cst c, double val) {
 #if SMOOTHEN_EQ == 0
     m_constants[c] = 0.9*m_constants[c] + 0.1*val*m_offset[c] ;
@@ -50,6 +68,9 @@ void Const::aim (cst c, double val) {
 #endif
 }
 
+/* Approximately the order of magnitude of a given constant.
+ * (except with a lower bound)
+ */
 double Const::orderOfMagnitude (cst c) {
     double pow = 1.0 ;
     if (abs(m_constants[c]) <= 1e-10) return 1.0 ;
